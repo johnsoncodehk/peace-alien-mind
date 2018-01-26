8ac34f952f6be2b1c61afc7fd1d80aa06b5cd8b9
johnsoncodehk <johnsoncodehk@gmail.com>
2018-01-27 04:52:47 +0800

    Add 試用Player

--- Assets/GGJ2018/Scripts/Player.cs
+++ Assets/GGJ2018/Scripts/Player.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	public float smoothTime;
+	public float maxSpeed;
+	public float deltaTime;
+
+	private Vector2 m_MoveVelocity;
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update() {
+		this.LookToMouse();
+		this.MoveToMouse();
+	}
+
+	private void LookToMouse() {
+		Vector3 targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector3 diff = targetPosition - this.transform.position;
+		diff.Normalize();
+		float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+		this.transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+	}
+	private void MoveToMouse() {
+		Vector3 targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		this.transform.position = Vector2.SmoothDamp(this.transform.position, targetPosition, ref this.m_MoveVelocity, this.smoothTime, this.maxSpeed, this.deltaTime);
 	}
 }
