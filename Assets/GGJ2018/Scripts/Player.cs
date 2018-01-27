d2f1206af678732fccf88d4559ad72d7594379e4
johnsoncodehk <johnsoncodehk@gmail.com>
2018-01-28 06:36:10 +0800

    Add 多人遊戲
    按空白鍵會加入新玩家

--- Assets/GGJ2018/Scripts/Player.cs
+++ Assets/GGJ2018/Scripts/Player.cs
@@ -4,28 +4,67 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+	public static List<Player> instances = new List<Player>();
+
 	public float smoothTime;
 	public float maxSpeed;
 	public float deltaTime;
 	public Signal signal;
 	public Transform shooter, attractPoint;
+	public bool isPlayerOne {
+		get { return Player.instances[0] == this; }
+	}
+	public bool isNewPlayer;
 
 	private Vector2 m_MoveVelocity;
 	
+	void Awake() {
+		Player.instances.Add(this);
+	}
 	void Update() {
-		this.LookToMouse();
-		this.MoveToMouse();
+		if (this.isNewPlayer) {
+			if (Vector3.Distance(this.transform.position, new Vector3(0, 0, 0)) > 10) {
+				this.LookTo(Vector3.zero);
+				this.MoveTo(Vector3.zero);
+				return;
+			}
+			this.isNewPlayer = false;
+		}
+		if (this.isPlayerOne) {
+			this.LookToMouse();
+			this.MoveToMouse();
+		}
+		else {
+			this.LookToInput();
+			this.MoveToInput();
+		}
 	}
 
 	private void LookToMouse() {
 		Vector3 targetPosition = GameManager.instance.mainCamera.ScreenToWorldPoint(Input.mousePosition);
+		this.LookTo(targetPosition);
+	}
+	private void MoveToMouse() {
+		Vector3 targetPosition = GameManager.instance.mainCamera.ScreenToWorldPoint(Input.mousePosition);
+		this.MoveTo(targetPosition);
+	}
+	private void LookToInput() {
+		Vector3 input = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+		Vector3 targetPosition = this.transform.position + input * 10;
+		this.LookTo(targetPosition);
+	}
+	private void MoveToInput() {
+		Vector3 input = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+		Vector3 targetPosition = this.transform.position + input * 10;
+		this.MoveTo(targetPosition);
+	}
+	private void LookTo(Vector3 targetPosition) {
 		Vector3 diff = targetPosition - this.transform.position;
 		diff.Normalize();
 		float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
 		this.transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
 	}
-	private void MoveToMouse() {
-		Vector3 targetPosition = GameManager.instance.mainCamera.ScreenToWorldPoint(Input.mousePosition);
+	private void MoveTo(Vector3 targetPosition) {
 		this.transform.position = Vector2.SmoothDamp(this.transform.position, targetPosition, ref this.m_MoveVelocity, this.smoothTime, this.maxSpeed, this.deltaTime);
 	}
 
