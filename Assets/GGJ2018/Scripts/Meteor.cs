2d8ae025e8d7e97e010c30900137c0ee0067bb4f
johnsoncodehk <johnsoncodehk@gmail.com>
2018-01-28 01:57:39 +0800

    Delete unused code

--- Assets/GGJ2018/Scripts/Meteor.cs
+++ Assets/GGJ2018/Scripts/Meteor.cs
@@ -4,16 +4,11 @@ using UnityEngine;
 
 public class Meteor : MonoBehaviour {
 
-	// public float startTime = 10;
 	public float lifeTime = 5;
 	public float gravity = 1;
 	public Vector2 speed;
 	
 	void Update() {
-		// if (this.startTime > 0) {
-		// 	this.startTime -= Time.deltaTime;
-		// 	return;
-		// }
 		this.speed.y -= this.gravity * Time.deltaTime;
 		this.transform.position += (Vector3)this.speed;
 		this.lifeTime -= Time.deltaTime;
