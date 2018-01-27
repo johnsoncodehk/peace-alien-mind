9c822fc7b359343824431f0b798791f9894c06af
StephenWong <14219328@life.hkbu.edu.hk>
2018-01-27 06:07:53 -0800

    planet

---
+++ Assets/GGJ2018/Scripts/Forward.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Forward : MonoBehaviour {
+
+    Rigidbody2D ball;
+
+
+	// Use this for initialization
+	void Start () {
+        ball = GetComponent<Rigidbody2D>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        ball.velocity = ball.transform.right * 1;
+
+		
+	}
+}
---
+++ Assets/GGJ2018/Scripts/Player2Testing.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Testing : MonoBehaviour {
+
+    Rigidbody2D planet;
+    public float speed;
+    public GameObject prefab;
+    public GameObject tower;
+
+    private float nextShootTime;
+
+	// Use this for initialization
+	void Start () {
+        planet = GetComponent<Rigidbody2D>();
+        this.nextShootTime = Random.Range(1, 2f);
+
+    }
+	
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Time.time >= this.nextShootTime)
+        {
+            this.nextShootTime += Random.Range(1, 2f);
+            Instantiate(prefab, tower.transform.position, planet.transform.rotation);
+        }
+        //if (Random.Range(0, 100) == 1)
+        //{
+        //    Instantiate(prefab, tower.transform.position , planet.transform.rotation);
+        //}
+        planet.MoveRotation( planet.rotation + speed *Time.deltaTime);
+	}
+}
