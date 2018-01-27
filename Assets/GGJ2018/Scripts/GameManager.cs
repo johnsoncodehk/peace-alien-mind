4e8766a000c9ae9570752c9b2cd2569bc8222e3f
johnsoncodehk <johnsoncodehk@gmail.com>
2018-01-27 18:16:27 +0800

    增加開場情境，加速發射塔速度

--- Assets/GGJ2018/Scripts/GameManager.cs
+++ Assets/GGJ2018/Scripts/GameManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+	public static GameManager instance;
+
 	public bool isWin {
 		get {
 			foreach (Planet planet in this.planets) {
@@ -17,12 +19,16 @@ public class GameManager : MonoBehaviour {
 
 	public int state;
 	public PlanetAxis planetAxis;
+	public Camera mainCamera;
 	
 	private Planet[] planets = new Planet[0];
 
 	void Awake() {
+		GameManager.instance = this;
 		this.planets = FindObjectsOfType<Planet>();
 		// StartCoroutine(this.WinAction()); // test
+
+		StartCoroutine(this.CameraZoon());
 	}
 	void Update() {
 		if (this.state == 0) {
@@ -32,9 +38,18 @@ public class GameManager : MonoBehaviour {
 		}
 	}
 
+	private IEnumerator CameraZoon() {
+		GameManager.instance.mainCamera.orthographicSize = 10;
+		yield return new WaitForSeconds(5);
+		float v = 0;
+		while (GameManager.instance.mainCamera.orthographicSize < 40) {
+			GameManager.instance.mainCamera.orthographicSize = Mathf.SmoothDamp(GameManager.instance.mainCamera.orthographicSize, 40, ref v, 10);
+			yield return new WaitForEndOfFrame();
+		}
+	}
 	private IEnumerator WinAction() {
 		yield return new WaitForSeconds(5);
-		Camera.main.orthographic = false;
+		GameManager.instance.mainCamera.orthographic = false;
 		this.planetAxis.gameObject.SetActive(true);
 		for (int i = 0; i < this.planets.Length; i++) {
 			Planet planet = this.planets[i];
--- Assets/GGJ2018/Scripts/Player.cs
+++ Assets/GGJ2018/Scripts/Player.cs
@@ -18,14 +18,14 @@ public class Player : MonoBehaviour {
 	}
 
 	private void LookToMouse() {
-		Vector3 targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector3 targetPosition = GameManager.instance.mainCamera.ScreenToWorldPoint(Input.mousePosition);
 		Vector3 diff = targetPosition - this.transform.position;
 		diff.Normalize();
 		float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
 		this.transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
 	}
 	private void MoveToMouse() {
-		Vector3 targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector3 targetPosition = GameManager.instance.mainCamera.ScreenToWorldPoint(Input.mousePosition);
 		this.transform.position = Vector2.SmoothDamp(this.transform.position, targetPosition, ref this.m_MoveVelocity, this.smoothTime, this.maxSpeed, this.deltaTime);
 	}
 
