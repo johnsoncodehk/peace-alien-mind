0cd54dd46201a0fa4a735a8748d27b9f336c90d3
johnsoncodehk <johnsoncodehk@gmail.com>
2018-01-27 12:31:55 +0800

    Scripts update

--- Assets/GGJ2018/Scripts/Planet.cs
+++ Assets/GGJ2018/Scripts/Planet.cs
@@ -26,7 +26,7 @@ public class Planet : MonoBehaviour {
 
 	public int levelCount = 4;
 	public List<GameObject> levelObjects = new List<GameObject>();
-	public Transform holder;
+	public Transform holder, ball;
 	public float rotationSpeed;
 	public float zPosSize = 0.2f;
 
@@ -55,7 +55,7 @@ public class Planet : MonoBehaviour {
 			pos.z = (pos.z - this.holder.parent.position.z) * zPosSize + this.holder.parent.position.z;
 			this.transform.position = pos;
 		}
-		this.levelObjects[0].transform.Rotate(new Vector3(0, 0, this.rotationSpeed * Time.deltaTime));
+		this.ball.Rotate(new Vector3(0, 0, this.rotationSpeed * Time.deltaTime));
 	}
 
 	public void LevelUp() {
--- Assets/GGJ2018/Scripts/Signal.cs
+++ Assets/GGJ2018/Scripts/Signal.cs
@@ -4,13 +4,54 @@ using UnityEngine;
 
 public class Signal : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	public AnimationCurve sizeCurve, colorCurve;
+	public Vector3 startSize, endSize;
+	public Color startColor, endColor;
+	public float lifeTime;
+	public bool isFirst;
+	[HideInInspector] public Signal child;
+	[HideInInspector] public Player player;
+
+	private float m_StartTime;
+	private SpriteRenderer m_SpriteRenderer;
+	private Rigidbody2D m_Rigidbody;
+	private Collider2D m_Collider;
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void Awake() {
+		this.m_StartTime = Time.time;
+		this.m_SpriteRenderer = this.GetComponent<SpriteRenderer>();
+		this.m_Rigidbody = this.GetComponent<Rigidbody2D>();
+		this.m_Collider = this.GetComponent<Collider2D>();
+
+		this.transform.localScale = Vector3.zero;
+	}
+	void Update() {
+		if (player) {
+			
+		}
+		else {
+			float currentTime = Time.time - this.m_StartTime;
+			float t = currentTime / lifeTime;
+			this.transform.localScale = Vector3.Lerp(this.startSize, this.endSize, this.sizeCurve.Evaluate(t));
+			this.m_SpriteRenderer.color = Color.Lerp(this.startColor, this.endColor, this.colorCurve.Evaluate(t));
+			this.m_Rigidbody.velocity = this.m_Rigidbody.transform.up * 1;
+		}
+	}
+	public void OnTriggerEnter2D(Collider2D other) {
+		Player player = other.GetComponent<Player>();
+		if (player && this.isFirst) {
+			this.ToPlayer(player);
+		}
+		Planet planet = other.GetComponent<Planet>();
+		if (planet) {
+			planet.LevelUp();
+		}
+	}
+
+	private void ToPlayer(Player player) {
+		this.player = player;
+		if (this.child) {
+			this.child.ToPlayer(player);
+		}
 	}
 }
