9446887a3d5fff1d2ecc3a07e0e3b1d573283144
johnsoncodehk <johnsoncodehk@gmail.com>
2018-03-03 16:40:44 +0800

    Call RedBull GameOver API

--- Assets/GGJ2018/Scripts/GameManager.cs
+++ Assets/GGJ2018/Scripts/GameManager.cs
@@ -23,6 +23,7 @@ public class GameManager : MonoBehaviour {
 	public Camera mainCamera;
 	public PostProcessingBehaviour postProcessing;
 	public bool showBlurry;
+	public GameData gameData;
 	// UI
 	public MainMenu mainMenu;
 	public SettingsPanel settingsPanel;
@@ -49,6 +50,7 @@ public class GameManager : MonoBehaviour {
 		});
 		this.backButtonImage = this.backButton.GetComponent<Image>();
 		this.postProcessing.profile = Instantiate(this.postProcessing.profile);
+		StartCoroutine(this.LoadGameData());
 	}
 	void Update() {
 		if (Input.GetButtonDown("Horizontal")) {
@@ -113,6 +115,20 @@ public class GameManager : MonoBehaviour {
 		StartCoroutine(this.PlayLevelAsync(level));
 	}
 
+	private IEnumerator LoadGameData() {
+		string filePath = System.IO.Path.Combine(Application.streamingAssetsPath, "game_data.txt");
+
+		string result;
+		if (filePath.Contains("://") || filePath.Contains(":///")) {
+			WWW www = new WWW(filePath);
+			yield return www;
+			result = www.text;
+		}
+		else
+			result = System.IO.File.ReadAllText(filePath);
+
+		print(result);
+	}
 	private IEnumerator PlayLevelAsync(int level) {
 		if (this.m_PlayingLevel.isLoaded) yield break;
 		if (this.loadingLevel) yield break;
---
+++ Assets/RedBullMindGamersPlatform/Plguins/RedBullMindGamersPlatform.jslib
@@ -0,0 +1,9 @@
+mergeInto(LibraryManager.library, {
+
+  GameOver: function (level, score) {
+    var msg = "gameOver:" + level + ":" + score;
+    console.log("postMessage: " + msg);
+    parent.postMessage(msg, "*");
+  },
+
+});
