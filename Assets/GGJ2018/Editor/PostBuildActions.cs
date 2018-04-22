26496468c6f323e384c64ab0facf79e3e1c784f9
johnsoncodehk <johnsoncodehk@gmail.com>
2018-04-22 23:56:41 +0800

    Remove all warring

--- Assets/GGJ2018/Editor/PostBuildActions.cs
+++ Assets/GGJ2018/Editor/PostBuildActions.cs
@@ -1,17 +1,16 @@
-﻿ using System;
- using System.IO;
- using System.Text.RegularExpressions;
- using UnityEditor;
- using UnityEditor.Callbacks;
- 
- public class PostBuildActions {
-	 [PostProcessBuild]
-	 public static void OnPostProcessBuild(BuildTarget target, string targetPath) {
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+public class PostBuildActions {
+	[PostProcessBuild]
+	public static void OnPostProcessBuild(BuildTarget target, string targetPath) {
 		var path = Path.Combine(targetPath, "Build/UnityLoader.js");
 		var text = File.ReadAllText(path);
-		// UnityLoader.SystemInfo.mobile?e.popup("Please note that Unity WebGL is not currently supported on mobiles. Press OK if you wish to continue anyway.",[{text:"OK",callback:t}]):
-		text = text.Replace("UnityLoader.SystemInfo.mobile?e.popup(\"Please note that Unity WebGL is not currently supported on mobiles. Press OK if you wish to continue anyway.\",[{text:\"OK\",callback:t}]):", "");
+		text = Regex.Replace(text, @"compatibilityCheck:function\(e,t,r\)\{.+,Blobs:\{\},loadCode",
+			"compatibilityCheck:function(e,t,r){t()},Blobs:{},loadCode");
 		File.WriteAllText(path, text);
-	 }
- }
- 
+	}
+}
